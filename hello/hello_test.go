35644cc410d639cfd98cd408cc476ae6f2be6fc6
Nguyen Tran Viet Anh <anh.ntv@teko.vn>
2024-05-06 17:57:37 +0700

    initial commit

---
+++ hello/hello_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Anh", "French")
+		want := "Bonjour, Anh"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Anh", "Spanish")
+		want := "Hola, Anh"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
